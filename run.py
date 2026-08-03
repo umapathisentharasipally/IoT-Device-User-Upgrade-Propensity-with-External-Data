82df5b57bba08f511386c8a1668dbe3d957cb0f5
Tech Support _ Umapathi S <Techsupport.umapathi@exafluence.com>
2026-08-03 15:37:00 +0530

    backend root

---
+++ run.py
@@ -0,0 +1,9 @@
+college = {"student1":{"name":"naveen", "department":"cse", "age":20},
+           "student2":{"name":"sai","department":"EEE", "age":21},
+           "student3":{"name":"ram","department":"ECE", "age":22},
+           "student4":{"name":"ravi","department":"MECH", "age":23}
+           }
+
+print(college)
+
+print(college["student1"]["name"])
